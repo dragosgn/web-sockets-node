2ecda72637bd1d7001d1a78d3cc404818f4657be
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-01-15 11:35:43 +0100

    responding to messages both server and client side

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-})
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-io.on('connection', function(socket){
-  console.log('a user is connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+io.on("connection", function(socket) {
+  socket.on("chat message", function(msg) {
+    io.emit("chat message", msg);
   });
-})
+});
 
-http.listen(3000, function(){
-  console.log('listening on port: 3000');
-})
+http.listen(3000, function() {
+  console.log("listening on port: 3000");
+});
